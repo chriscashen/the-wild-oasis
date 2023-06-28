597f438b8756d5d077cb5c2dea58595e98e2ed03
chriscashen <chris.cashen@hotmail.com>
2023-06-28 09:29:30 -0700

    add page routes

---
+++ src/ui/Input.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+border: 1px solid var(--color-grey-300);
+background-color: var(--color-grey-0);
+border-radius: var(--border-radius-sm);
+padding: .8rem 5.2rem;
+box-shadow: var(--box-shadow--sm);
+`;
+
+
+export default Input;
