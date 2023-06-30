a981104d5acad72b242e8b5522857574707a2be9
chriscashen <chris.cashen@hotmail.com>
2023-06-29 19:20:32 -0700

    add react-hook-form

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,15 +5,23 @@ import Header from "./Header";
 import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
-display: grid;
-height: 100vh;
-grid-template-columns: 26rem 1fr;
-grid-template-rows: auto 1fr;
-`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
 `;
 
 function AppLayout() {
@@ -22,7 +30,9 @@ function AppLayout() {
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
