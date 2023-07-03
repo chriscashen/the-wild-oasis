aa3074839613360945579bee89d774bf529151e4
chriscashen <chris.cashen@hotmail.com>
2023-07-03 07:47:30 -0700

    add FormRow component

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -12,7 +12,20 @@ export async function getCabins() {
 }
 
 export async function deleteCabin(id) {
-  const { data,error } = await supabase.from("cabins").delete().eq("id", id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(`cabins error: ${error.message}`);
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
 
   if (error) {
     console.log(`cabins error: ${error.message}`);
